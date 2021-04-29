09d4ba10536a95608387c49e5fec1e652d906da5
svenson.e <svenson.ertel@gmail.com>
2021-04-30 04:15:21 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Fastai Image Recognition App
+
+
+Fastai/ Pytorch Computer Vision
+
+- Architecture: ResNet34
+- Dataset: 
+- accuracy:
+
+
+deployed on share.io with streamlit
+https://share.streamlit.io/svensone/fastai2-bali-dance/main/app.py
+
---
+++ app.py
@@ -0,0 +1,148 @@
+
+# from fastai.learner import load_learner
+from fastai.basics import *
+from fastai.vision.all import *
+import torchvision.transforms as T
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+import os
+import matplotlib.image as mpimg
+import requests
+from io import BytesIO
+from pathlib import Path
+import pathlib
+import base64
+from PIL import Image
+import PIL.Image
+
+## get local css
+##################
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+local_css("style.css")
+
+# adjustment for different systems (share.io PosixPath)
+# plt = platform.system()
+# if plt == 'Linux': 
+#     pathlib.WindowsPath = pathlib.PosixPath
+
+## Layout App
+##################
+st.title('**Balinese Dance Style** - Classification')
+st.markdown("""
+### AI - Computer Vision Recognition with **fastai/pytorch**
+#### Classifing images of famous balinese dances styles: *Kecak* , *Barong*  &  *Legong*
+**Dataset**: 500 Images scraped from Web, cleaned and preprocessed.
+**Accuracy** on ResNet34 Architectur: 80% 
+""")
+link1 = 'Model & Data Preprocessing [Github](https://github.com/Svensone/fastAI2-Bali-Dance/blob/main/2021_03_29_%5Bfastaiv2%5D_New_Balinese_Dance_Image_Recognition.ipynb)'
+link2 = 'Deployment [Github](https://github.com/Svensone/fastAI2-Bali-Dance)'
+st.markdown(link1, unsafe_allow_html=True, )
+st.markdown(link2, unsafe_allow_html=True)
+
+# Set Background Image *local file"
+###################################################
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+        with open(bin_file, 'rb') as f:
+            data = f.read()
+        return base64.b64encode(data).decode()
+def set_png_as_page_bg(png_file):
+        bin_str = get_base64_of_bin_file(png_file)
+        page_bg_img = '''
+        <style>
+        body {
+        background-image: url("data:image/png;base64,%s");
+        background-size: cover;
+        }
+        </style>
+        ''' % bin_str
+        st.markdown(page_bg_img, unsafe_allow_html=True)
+        return
+PATH = pathlib.Path(__file__).parent
+IMG_PATH = PATH.joinpath("assets").resolve()
+set_png_as_page_bg(IMG_PATH.joinpath('bg2.jpeg'))
+
+#######################################
+### Image Classification
+#######################################
+def prediction(img, display_img):
+    # display the image
+    st.image(display_img, use_column_width=True)
+    # loading spinner
+    with st.spinner('Wait a second .....'):
+        time.sleep(3)
+    # Option 1. setup model & load weights
+    # DATA_PATH = PATH.joinpath('data')
+    # CSV_PATH = DATA_PATH.joinpath('cleaned.csv')
+    # MODEL_PATH  = DATA_PATH.joinpath('models', 'v2-stage-1.pth')
+    # data = ImageDataLoaders.from_csv(path=DATA_PATH, csv_fname='cleaned.csv', valid_pct=0.2, item_tfms=Resize(224), csv_labels='cleaned.csv', bs=64)
+    # learn = cnn_learner(data, models.resnet34, metrics=accuracy)
+    # learn.load('v2-stage-1')
+    # Option 2:
+    learn = load_learner('v2-resnet34.pkl')
+
+    # Prediction on Image
+    predict_class = learn.predict(img)[0]
+    predict_proba = learn.predict(img)[2]
+    # print(predict_class, predict_proba)
+
+    # Display results
+    if str(predict_class) == 'legong':
+        st.success(f'This is a Scene of the famous Legong Kraton Dance. Probability of Prediction is {round(float(predict_proba[2])*100, 2)} % ')
+        link = 'Find out more [Wikipedia](https://en.wikipedia.org/wiki/Legong)'
+        st.markdown(link, unsafe_allow_html=True)
+    elif str(predict_class) == "barong":
+        st.success(f'Barong Dance. Barong is, together with Sanghyang, considered to be an ancient native Balinese Dance. Probability of Prediction is {round(float(predict_proba[0])*100, 2)} %')
+        link = '[Barong Wikipedia](https://en.wikipedia.org/wiki/Barong_(mythology)#Barong_dance)'
+        st.markdown(link, unsafe_allow_html=True)
+    else:
+        st.success(f'This is a Scene of the Kecak Dance, created by german artist Walter Spies in 1930s. Probability of Prediction is {round((float(predict_proba[1])*100), 2)} % ')
+        link = '[Kecak Wikipedia](https://en.wikipedia.org/wiki/Kecak)'
+        st.markdown(link, unsafe_allow_html=True)
+
+#######################################
+### Image Selection
+#######################################
+option1= 'Choose a test image from list'
+option2= 'Predict your own Image'
+option = st.radio('', [option1, option2 ])
+if option == option1:
+    # Select an image
+    list_test_img = os.listdir('test')
+    test_img = st.selectbox(
+        'Please select an image:', list_test_img)
+    # Read the image
+    TEST_IMG_PATH = PATH.joinpath('test', test_img)
+    file_path = 'test/'+ test_img
+    img = PILImage.create(TEST_IMG_PATH)
+
+    im_test3 = PIL.Image.open(TEST_IMG_PATH)
+    display_img = np.asarray(im_test3) # Image to display
+    # call predict func with this img as parameters
+    prediction(img, display_img)
+
+## Predition from URL Image not yet working - converting to fastAI Image object error
+##################################################
+else:
+    url = st.text_input('URL of the image')
+    if url !='':
+        # print(url)
+        try:
+            # test url pic
+            # https://volunteerprogramsbali.org/wp-content/uploads/2015/11/news-108.jpg
+            # Read image from the url
+            response = requests.get(url)
+            pil_img = PIL.Image.open(BytesIO(response.content))
+            display_img = np.asarray(pil_img) # Image to display
+            # Transform the image
+            timg = TensorImage(image2tensor(pil_img))
+            tpil = PILImage.create(timg)
+            # call predict func
+            prediction(tpil, display_img)
+        except:
+            st.text("Invalid URL")
